cee47c9cec39066266e9554af24b41a046edb886
DavidStahl97 <d.stahl@tus-brandoberndorf.de>
2016-11-14 20:39:45 +0100

    started working on sessioninitializer with WCF

--- VoIPApp/VoIPApp.Modules.Chat/ChatModule.cs
+++ VoIPApp/VoIPApp.Modules.Chat/ChatModule.cs
@@ -8,6 +8,9 @@ using System.ComponentModel;
 using VoIPApp.Modules.Chat.Services;
 using VoIPApp.Modules.Chat.ViewModels;
 using VoIPApp.Modules.Chat.Views;
+using VoiceSessionInitializerLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 
 namespace VoIPApp.Modules.Chat
 {
@@ -17,9 +20,11 @@ namespace VoIPApp.Modules.Chat
         private readonly IUnityContainer container;
         private readonly AudioStreamingService audioStreamer;
         private readonly BackgroundWorker audioInitWorker;
+        private readonly BackgroundWorker sessionInitWorker;
         private readonly IFriendsService chatService;
         private readonly IMessageService messageService;
         private readonly IVoIPService voIPService;
+        private SessionInitializerService sessionInitializerService;
 
         public ChatModule(IRegionManager regionManager, IUnityContainer container, AudioStreamingService audioStreamer)
         {
@@ -35,6 +40,9 @@ namespace VoIPApp.Modules.Chat
 
             this.audioInitWorker = new BackgroundWorker();
             audioInitWorker.DoWork += audioInitWorker_DoWork;
+
+            this.sessionInitWorker = new BackgroundWorker();
+            sessionInitWorker.DoWork += sessionInitWorker_DoWork;
         }
 
         public void Initialize()
@@ -46,11 +54,36 @@ namespace VoIPApp.Modules.Chat
             this.container.RegisterType<object, ChatView>(NavigationURIs.chatViewUri.OriginalString);
 
             audioInitWorker.RunWorkerAsync();
+            sessionInitWorker.RunWorkerAsync();
         }
 
         private void audioInitWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             audioStreamer.Init();    
+        }
+
+        private void sessionInitWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.sessionInitializerService = new SessionInitializerService();
+
+            Uri baseAddress = new Uri("http://localhost:8000/VoIPApp/");
+            ServiceHost host = new ServiceHost(sessionInitializerService, baseAddress);
+
+            try
+            {
+                host.AddServiceEndpoint(typeof(ISessionInitializerService), new WSHttpBinding(), "SessionInitialService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+
+                host.Open();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine(ce.ToString());
+                host.Abort();
+            }
         }
     }
 }
---
+++ VoIPApp/VoiceSessionInitializerLibrary/ISessionInitializerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace VoiceSessionInitializerLibrary
+{
+    [ServiceContract]
+    public interface ISessionInitializerService
+    {
+        [OperationContract]
+        void Call(int friendID);
+    }
+}
